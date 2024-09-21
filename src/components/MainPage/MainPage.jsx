fbdbc7dc21c41084e908c4542c4264a6e4af8ffc
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2024-09-21 16:35:01 +0200

    changes added

---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+import "./style.scss";
+
+function MainPage() {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 1.1 }}
+      // transition={{ duration: 1 }}
+      dragConstraints={{ left: -100, right: 100 }}
+      className="animatedBlock"
+    >
+      <p>Hello world!</p>
+    </motion.div>
+  );
+}
+
+export default MainPage;
