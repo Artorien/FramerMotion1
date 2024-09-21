e13b6ae89474b3bfe33bb735cab94b83f62d8af1
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2024-09-21 16:46:02 +0200

    some changes

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,11 +6,11 @@ function MainPage() {
     <motion.div
       whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 1.1 }}
-      // transition={{ duration: 1 }}
+      transition={{ duration: 1 }}
       dragConstraints={{ left: -100, right: 100 }}
       className="animatedBlock"
     >
-      <p>Hello world!</p>
+      <p>Hello world! I am here</p>
     </motion.div>
   );
 }
