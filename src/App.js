6b6aed7f4d2258f3e9e076a6e86751b8ae6dcd04
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2024-09-22 19:33:07 +0200

    made improvements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./index.css";
 import MainPage from "./components/MainPage/MainPage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { createContext } from "react";
+
+// const [darkMode, setDarkMode] = createContext(); 
 
 function App() {
-  return <MainPage></MainPage>;
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <MainPage></MainPage>
+        <Footer></Footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,5 +1,7 @@
 import { motion, useMotionValue, useTransform } from "framer-motion";
 import "./style.scss";
+import Tesla from "../Images/Tesla.png";
+import Globe from "../Globe/Globe";
 
 function MainPage() {
   const x = useMotionValue(0);
@@ -7,19 +9,62 @@ function MainPage() {
 
   return (
     <>
-      <div className="hWrapper">
-        <h2>
-          This text is
-          <div className="wrapper">
-            <div className="container">
-              <div className="item item1">amazing</div>
-              <div className="item item2">stunning</div>
-              <div className="item item3">fantastic</div>
-              <div className="item item4">amazing</div>
+      <div className="grid grid-cols-[1fr_4fr_1fr] justify-center pt-40">
+        <motion.div
+          className="hWrapper col-start-2 h-full h-96 flex justify-between items-center"
+          grid-col
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <div>
+            <h2 className="font-light, text-3xl">
+              We provide
+              <div className="wrapper overflow-hidden h-10 inline-grid mx-2">
+                <div className="container">
+                  <div className="item item1">amazing</div>
+                  <div className="item item2">reliable</div>
+                  <div className="item item3">fantastic</div>
+                  <div className="item item4">amazing</div>
+                </div>
+              </div>
+              cars around the world
+            </h2>
+          </div>
+          <div className="w-9/12 h-full overflow-hidden relative">
+            <img
+              className="w-full object-cover right-[-32px] absolute top-0"
+              src={Tesla}
+            ></img>
+          </div>
+        </motion.div>
+        <motion.div
+          className="col-start-2 h-96 py-8 aboutUsContainer mt-28 rounded-xl pt-20"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <div className="grid grid-cols-[1fr_1fr] border-dotted border-b-2 pb-20">
+            <div className="col-start-1 border-r-2 flex justify-center">
+              <Globe></Globe>
+            </div>
+            <div className="col-start-2 flex justify-center flex-col items-center">
+              <div className="w-8/12 flex justify-center flex-col">
+                <h2 className="text-3xl pb-2">About Us</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repellendus, sed. Saepe, reprehenderit architecto! Veniam
+                  ullam laudantium voluptatem. Quis optio voluptate dolorum
+                  corrupti! Dolor sequi quia ipsam soluta tenetur minima
+                  repellendus?
+                </p>
+              </div>
             </div>
           </div>
-          and this is why You will like it!
-        </h2>
+          <div>
+            <p>hjjwhbcbwjhbce</p>
+          </div>
+        </motion.div>
       </div>
     </>
   );
